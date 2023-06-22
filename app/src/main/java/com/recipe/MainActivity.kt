e163beb8ed32c843e318dacdae2f1fd934e2ef81
tonnymuchui <tonykanyingah@gmail.com>
2023-06-23 00:17:40 +0300

    Add fragment

--- app/src/main/java/com/recipe/MainActivity.kt
+++ app/src/main/java/com/recipe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.recipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btn_nav)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
